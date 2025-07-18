5a77d02e5aa51c3d26f3c0f0526095ade0f99839
deep <166912166+de-ep@users.noreply.github.com>
2025-07-18 08:04:47 -0400

    some refactoring

---
+++ src/emulator/cpu.rs
@@ -0,0 +1,56 @@
+const MAX_REGS: usize = 32;
+
+pub enum RegType {
+    GenPurpose,
+    ProgCounter,
+}
+
+pub struct Cpu {
+    r: [u64; MAX_REGS],
+    pc: u64,
+}
+
+impl Cpu {
+    pub fn new() -> Self{
+        Cpu {
+            r: [0; MAX_REGS],
+            pc: 0,
+        }
+    }
+
+    pub fn get_reg(&self, rtype: RegType, reg_index: usize) -> Option<u64> {
+        match rtype {
+            RegType::GenPurpose => {
+                if reg_index >= MAX_REGS {
+                    return None;
+                }
+
+                Some(self.r[reg_index])
+            }
+            RegType::ProgCounter => {
+                Some(self.pc)
+            }
+            _=> unreachable!(),
+        }
+        
+    }
+
+    pub fn set_reg(&mut self, rtype: RegType, reg_index: usize, value: u64) -> Result<(), ()>{
+        match rtype {
+            RegType::GenPurpose => {
+                if reg_index >= MAX_REGS {
+                    return Err(());
+                }
+
+                self.r[reg_index] = value;
+            }
+            RegType::ProgCounter => {
+                self.pc = value;
+            }
+            _=> unreachable!(),
+        }
+        
+        Ok(())
+    }
+
+}
